8ab7febc8fb46bc60de86efb65f3c5b74d04d097
Antonides33 <anton.naters@digitalcareerinstitute.org>
2025-01-07 16:22:27 +0100

    Create guessthenumber.py

---
+++ guessthenumber.py
@@ -0,0 +1,21 @@
+import random
+
+number=random.randint(1,1000)
+
+guess=int(input("Guess the number: 1-1000 "))
+
+counter=0
+
+while guess != number & counter <=10:
+    if guess < number:
+        print("Your guess is lower than the number. Try again ")
+        
+
+    elif guess > number:
+        print("Your guess is higher than the number. Try again ")
+        
+    
+    guess = int(input("Guess again: "))
+    counter=counter + 1
+
+print (f"Out of tries. The number was: {number}")
